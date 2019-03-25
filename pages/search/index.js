8a28b9fc50d0b7c57b61bbd52a6dfed54ec94698
wanls4583 <409051987@qq.com>
2019-03-25 23:55:51 +0800

    搜索历史

--- pages/search/index.js
+++ pages/search/index.js
@@ -12,11 +12,16 @@ Page({
         size: 20,
         showPlace: false,
         showCancel: false,
-        focus: false
+        showHistory: true,
+        autoFocus: false,
+        history: []
     },
     onLoad: function() {
+        var history = wx.getStorageSync('search_history');
+        history = history && JSON.parse(history) || [];
         this.setData({
-            focus: true
+            autoFocus: true,
+            history: history
         });
     },
     //跳转到动漫详情页
@@ -26,39 +31,77 @@ Page({
             url: '/pages/detail/index?comic=' + encodeURIComponent(JSON.stringify(comic))
         });
     },
-    clickSearch() {
+    //选中搜索历史
+    selectHistory(e) {
+        var text = e.currentTarget.dataset.text;
         this.setData({
-            showPlace: false
+            searchKey: text,
+            comicList: [],
+            total: -1,
+            page: 1,
+            showCancel: true,
+            showPlace: false,
+            showHistory: false
+        });
+        this.getComicList();
+    },
+    //清除历史记录
+    clearHistory() {
+        wx.removeStorageSync('search_history');
+        this.setData({
+            history: []
+        });
+    },
+    //点击搜索框
+    clickSearch() {
+        
+    },
+    //输入内容
+    searchInput(e) {
+        this.setData({
+            searchKey: e.detail.value
         })
     },
+    //确认搜索
     searchConfirm(e) {
         this.setData({
             searchKey: e.detail.value,
             comicList: [],
             total: -1,
             page: 1,
-            showCancel: true
+            showCancel: true,
+            showHistory: false,
         });
+        if (this.data.searchKey && this.data.history.indexOf(this.data.searchKey) == -1) {
+            this.setData({
+                history: this.data.history.concat([this.data.searchKey])
+            })
+            wx.setStorageSync('search_history', JSON.stringify(this.data.history))
+        }
         this.getComicList();
     },
+    //取消搜索
     searchCancel() {
+        wx.navigateBack({
+            delta: 1
+        })
+    },
+    //聚焦
+    searchFocus() {
         this.setData({
-            searchKey: '',
-            comicList: [],
-            total: -1,
-            page: 1,
-            showCancel: false
-        });
+            showPlace: false,
+            showHistory: true
+        })
     },
+    //失焦
     searchBlur() {
-        setTimeout(() => {
-            if (!this.data.searchKey) {
-                this.setData({
-                    showPlace: true
-                });
-            }
-        }, 100);
+        if (!this.data.searchKey) {
+            this.setData({
+                showPlace: true
+            });
+        }
     },
+    //加载更多
     loadMore() {
         if (this.data.comicList.length && this.data.comicList.length >= this.data.total) {
             return;
@@ -69,6 +112,7 @@ Page({
         }
         this.getComicList();
     },
+    //加载列表
     getComicList() {
         var self = this;
         wx.request({
