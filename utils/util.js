14e673fe1ea88ea16ff5f7d99803bd77b5f7bde3
wanls4583 <409051987@qq.com>
2019-03-26 20:21:52 +0800

    时间格式bugfix

--- utils/util.js
+++ utils/util.js
@@ -21,21 +21,21 @@ function passTime(date) {
     date = date instanceof Date ? date : new Date(date);
     var now = new Date();
     var todayTs = now - now.getHours() * 60 * 60 * 1000 - now.getMinutes() * 60 * 1000 - now.getSeconds() * 1000 - now.getMilliseconds();
-    var differ = now - date;
+    var differ = todayTs - date;
     var hours = date.getHours();
     var minutes = date.getMinutes();
     hours = ('0' + hours).slice(-2);
     minutes = ('0' + minutes).slice(-2);
-    if (differ < 60 * 60 * 1000) {
-        return Math.ceil(differ / 60 / 1000) + '分钟前更新';
-    } else if (differ < 24 * 60 * 60 * 1000) {
-        return '今天 ' + hours + ':' + minutes + '更新'
-    } else if (differ < 2 * 24 * 60 * 60 * 1000) {
-        return '昨天 ' + hours + ':' + minutes + '更新';
-    } else if (differ < 3 * 24 * 60 * 60 * 1000) {
-        return '昨天 ' + hours + ':' + minutes + '更新';
-    } else {
+    if (differ > 2 * 24 * 60 * 60 * 1000) {
         return formatTime(date, 'yyyy/MM/dd') + '更新';
+    } else if (differ > 1 * 24 * 60 * 60 * 1000) {
+        return '前天 ' + hours + ':' + minutes + '更新';
+    } else if(differ > 0) {
+        return '昨天 ' + hours + ':' + minutes + '更新';
+    } else if(now - date < 60*60*1000) {
+        return Math.ceil((now - date) / 60 / 1000) + '分钟前更新';
+    } else {
+        return '今天 ' + hours + ':' + minutes + '更新';
     }
 }
 
