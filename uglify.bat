a595ef8ae039e6187820423d1fcc489851c2bc31
wanls4583 <409051987@qq.com>
2019-03-31 00:53:36 +0800

    批处理压缩

---
+++ uglify.bat
@@ -0,0 +1,6 @@
+call uglifyjs ./pages/index/index.js -o ./pages/index/index.js -c -m
+call uglifyjs ./pages/detail/index.js -o ./pages/detail/index.js -c -m
+call uglifyjs ./pages/center/index.js -o ./pages/center/index.js -c -m
+call uglifyjs ./pages/picture/index.js -o ./pages/picture/index.js -c -m
+call uglifyjs ./pages/history/index.js -o ./pages/history/index.js -c -m
+call uglifyjs ./pages/search/index.js -o ./pages/search/index.js -c -m
