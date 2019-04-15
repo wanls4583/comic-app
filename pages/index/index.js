3c75b08916615e8331957dd8cb689f33bdfd6021
wanls4583 <409051987@qq.com>
2019-04-15 20:37:09 +0800

    无限列表优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -24,10 +24,13 @@ Page({
         swiperData: [{}],
         renderData: [],
         swiperDataMap: [],
-        scrollTop: []
+        scrollTop: [],
+        palceholderHeight: {}
     },
     onLoad: function() {
         wx.hideTabBar();
+        this.itemHeight = 175 * app.globalData.systemInfo.screenWidth / 375;
+        this.maxTopHeight = 200;
         this.loading = {};
         this.getCategory();
         this.getRecommend();
@@ -46,8 +49,55 @@ Page({
     //滚动事件
     onScroll(e) {
         var cid = e.currentTarget.dataset.cid;
-        //记录当前滚动距离
+        var swiperData = this.data.swiperData[cid];
         this.data.swiperData[cid].scrollTop = e.detail.scrollTop;
+        if (!cid) {
+            return;
+        }
+        if (!this.deling && e.detail.scrollTop > this.maxTopHeight * this.itemHeight) {
+            var key = `swiperData[${cid}].renderData`;
+            var total = `swiperData[${cid}].total`;
+            var delCount = Math.floor(e.detail.scrollTop / this.itemHeight) * 3 - 3 * this.maxTopHeight / 2;
+            this.deling = true;
+            swiperData.startIndex += delCount;
+            console.log('delCount', delCount);
+            this.setData({
+                [key]: swiperData.renderData.slice(delCount),
+                [total]: swiperData.total,
+                [`scrollTop[${cid}]`]: e.detail.scrollTop - delCount / 3 * this.itemHeight
+            }, () => {
+                this.setData({
+                    [`scrollTop[${cid}]`]: e.detail.scrollTop - delCount / 3 * this.itemHeight
+                }, () => {
+                    setTimeout(() => {
+                        this.deling = false;
+                    }, 1000);
+                });
+            });
+        } else if (!this.deling && swiperData.startIndex > 0 && e.detail.scrollTop < this.maxTopHeight / 2 * this.itemHeight - 3) {
+            var key = `swiperData[${cid}].renderData`;
+            var startIndex = swiperData.startIndex - this.maxTopHeight / 2 * 3;
+            startIndex = startIndex > 0 ? startIndex : 0;
+            var addData = swiperData.list.slice(startIndex, swiperData.startIndex);
+            swiperData.startIndex = startIndex;
+            this.deling = true;
+            console.log('addData', addData.length);
+            if (swiperData.endIndex - startIndex > 2 * this.maxTopHeight) {
+                swiperData.endIndex = 2 * this.maxTopHeight + startIndex;
+            }
+            this.setData({
+                [key]: swiperData.list.slice(startIndex, swiperData.endIndex),
+                [`scrollTop[${cid}]`]: e.detail.scrollTop + addData.length / 3 * this.itemHeight
+            }, () => {
+                this.setData({
+                    [`scrollTop[${cid}]`]: e.detail.scrollTop + addData.length / 3 * this.itemHeight
+                }, () => {
+                    setTimeout(() => {
+                        this.deling = false;
+                    }, 1000);
+                });
+            });
+        }
     },
     //加载更多
     onLoadMore(e) {
@@ -216,27 +266,25 @@ Page({
     },
     //刷新分类列表
     refreshCategory() {
-        this.swiperData[this.data.nowCid] = null;
+        this.data.swiperData[this.data.nowCid] = null;
         this.renderSwiper(this.data.nowCidIndex);
-    },
-    //顶部加载
-    loadPre(cid) {
-        
     },
     //底部加载
     loadNext(cid) {
         var swiperData = this.data.swiperData[cid];
         if (swiperData && swiperData.endIndex < swiperData.list.length) {
-            var addData = swiperData.list.slice(swiperData.endIndex, swiperData.endIndex+90);
+            var addData = swiperData.list.slice(swiperData.endIndex, swiperData.endIndex + 90);
             var key = `swiperData[${cid}].renderData`;
             var total = `swiperData[${cid}].total`;
+            var endIndex = `swiperData[${cid}].endIndex`;
             swiperData.endIndex += addData.length;
             this.setData({
                 [key]: swiperData.renderData.concat(addData),
-                [total]: swiperData.total
+                [total]: swiperData.total,
+                [endIndex]: swiperData.endIndex
             });
-        } else if(!this.loading[cid] && (!swiperData || swiperData.total < 0 || swiperData.list.length < swiperData.total)){
-            if(!swiperData) {
+        } else if (!this.loading[cid] && (!swiperData || swiperData.total < 0 || swiperData.list.length < swiperData.total)) {
+            if (!swiperData) {
                 this.data.swiperData[cid] = {
                     scrollTop: 0,
                     total: -1,
@@ -248,7 +296,7 @@ Page({
                 }
                 swiperData = this.data.swiperData[cid];
             }
-            this.getComicListByCategory(cid).then((data)=>{
+            this.getComicListByCategory(cid).then((data) => {
                 swiperData.list = swiperData.list.concat(data.list);
                 swiperData.total = data.size;
                 swiperData.page++;
