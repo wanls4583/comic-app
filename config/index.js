5af2f9d8f5a44b2ccfa1a788a9fd1d9d33ea6a53
wanls4583 <409051987@qq.com>
2020-06-14 11:48:42 +0800

    [master]-wx:key修改

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // httpHost: 'https://lisong.hn.cn',
+    httpHost: 'https://lisong.hn.cn',
     // httpHost: 'http://localhost:3000',
-    httpHost: 'http://lisong.hn.cn:3000',
+    // httpHost: 'http://lisong.hn.cn:3000',
     // httpHost: 'https://49.235.108.87'
 }
