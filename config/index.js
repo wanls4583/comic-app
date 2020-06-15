548527aa6a9cd121b627c8ccdb80173ad6c503ca
wanls4583 <409051987@qq.com>
2020-06-15 15:41:22 +0800

    [master]-请求路径修改

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-    httpHost: 'https://lisong.hn.cn',
-    // httpHost: 'http://localhost:3000',
-    // httpHost: 'http://lisong.hn.cn:3000',
-    // httpHost: 'https://49.235.108.87'
+    httpHost: 'https://lisong.hn.cn/comic_server',
+    // httpHost: 'http://localhost:3000/comic_server',
+    // httpHost: 'http://lisong.hn.cn:3000/comic_server',
+    // httpHost: 'https://49.235.108.87/comic_server'
 }
